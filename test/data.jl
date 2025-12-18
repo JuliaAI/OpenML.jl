b545e1a1394d01459a08b213aee1a94422980f3f
Anthony D. Blaom <anthony.blaom@gmail.com>
2025-12-19 09:19:36 +1300

    rm more of the same

--- test/data.jl
+++ test/data.jl
@@ -30,11 +30,11 @@ end
 @testset "data api functions" begin
     @test typeof(dqlist_test["data_qualities_list"]) <: Dict
 
-    @test typeof(data_features_test) <: Dict
+#    @test typeof(data_features_test) <: Dict
     @test length(data_features_test["data_features"]["feature"]) == 5
     @test data_features_test["data_features"]["feature"][1]["name"] == "sepallength"
 
-    @test typeof(data_qualities_test) <: Dict
+#    @test typeof(data_qualities_test) <: Dict
 
     @test length(filters_test["data"]["dataset"]) == limit
     @test length(filters_test["data"]["dataset"][1]) == offset
